38502d43b405e5d5a7f2e03e7cc28e562cd8b8c3
Agastya LD Khatua <k.agastya01@gmail.com>
2026-07-18 21:06:29 +0530

    Refactor main method to use pattern function

--- main.java
+++ main.java
@@ -1,21 +1,24 @@
 import java.util.*;
 public class main{
+    public static int pattern(int n){
+    	for(int i=1; i<=n; i++){
+    		for(int j=1; j<=n-i; j++){
+    			System.out.print(" ");
+    		}
+    		for(int j=1; j<=i; j++){
+    			System.out.print(" *");
+    		}
+    		System.out.println();
+    	}
+    	return n;
+    }
+
 	public static void main(String[] args){
-		Scanner x = new Scanner(System.in);
-		System.out.print("Enter the value of 'n': ");
-		int n = x.nextInt();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter 'n': ");
+		int n = sc.nextInt();
 		System.out.println();
-		//Main loop
-		for(int i=1; i<=n; i++){
-		    //spaces
-		    for(int j=1; j<=n-i; j++){
-		    	System.out.print(" ");
-		    }
-			//pattern
-			for(int j=1; j<=i; j++){
-				System.out.print(" *");
-			}
-			System.out.println();
-		}
+		
+		pattern(n);
 	}
 }
